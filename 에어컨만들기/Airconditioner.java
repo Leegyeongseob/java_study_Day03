e4e8909544f45713515eee22481c9e8c95265c79
Neo.경섭 <67867076+Leegyeongseob@users.noreply.github.com>
2024-03-19 11:09:19 +0900

    Add files via upload

---
+++ 에어컨만들기/Airconditioner.java
@@ -0,0 +1,19 @@
+package 에어컨만들기;
+// 전원 ON/OFF 가능
+// 온도 설정 기능 (1도 단위 설정)
+// 현재 온도 표시 기능
+// 냉방기 ON/OFF 기능
+// 바람세기 설정(1단계/2단계/3단계)
+
+
+//설정된 온도에 따라 온도 조정 가능 (60초/30초/20초) 추가로 온도 변함
+//에어컨 ON/OFF 동작을 위해 Scanner 입력에 대한 문자열 체크
+//현재 온도 표시를 위한 캘린더 API 활용(현재 날짜에 대한 적절한 온도 설정)
+
+public class Airconditioner {
+    boolean isPower;
+    int setTemp;
+    int currtemp;
+
+
+}
